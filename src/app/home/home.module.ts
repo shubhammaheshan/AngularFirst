b44e688f06a983d8d271b4bb48e8799c036b7856
Shubh <Shubhmaheshan1@gmail.com>
2020-04-22 19:25:07 +0530

    table

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { Routes } from '@angular/router';
+import { FooterComponent } from './Footer/footer.component';
 
 
 const homeRoutes:Routes = [
   {path:'', component:HomeComponent}
 ]
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent,FooterComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[FooterComponent]
+
 })
 export class HomeModule { }
